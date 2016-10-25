b99b7dfda9a8804918fc657cddf32fc4c3884c0e
Mokey <McClainG9616@Gmail.com>
2016-10-25 08:16:56 -0400

    MYSQL_Conn_App

---
+++ First_cSharp_SQL_APP/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace First_cSharp_SQL_APP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string myConnection = "datasource=127.0.0.1;port=3306;username=root;password=12345";
+                MySqlConnection myConn = new MySqlConnection(myConnection);
+                MySqlDataAdapter myDataAdapter = new MySqlDataAdapter();
+                myDataAdapter.SelectCommand = new MySqlCommand(" select * bans.bans ;", myConn);
+                MySqlCommandBuilder cb = new MySqlCommandBuilder(myDataAdapter);
+                myConn.Open();
+                MessageBox.Show("Connected!");
+                myConn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
